efa43ed49a37ec93c16b2b73c33eac973aedf088
Adam Gaunt <amd.gaunt@gmail.com>
2019-06-05 22:01:58 -0600

    Added body layout

--- my-app01/src/App.js
+++ my-app01/src/App.js
@@ -14,15 +14,88 @@ function App() {
       <Header />
       <main>
         <section className="m-auto">
-          <div className="icon-banner p-3">
+        <div className="container">
+          <div className="icon-banner">
             <div className="icons d-flex justify-content-around">
               <i className="fa fa-camera fa-3x p-3"></i>
+              <i className="fa fa-envelope fa-3x p-3"></i>
+              <i className="fa fa-signal fa-3x p-3"></i>
+              <i className="fa fa-wrench fa-3x p-3"></i>
+            </div>
+          </div>
+          </div>
+        </section>
+
+        <section className="m-auto">
+        <div className="container">
+          <div className="icons d-flex justify-content-around p-3">
+            <i className="fa fa-camera fa-3x p-3"></i>
+          </div>
+          <div className="d-flex justify-content-center text-center p-3">
+            <h1>Welcome to the Coach Approach for Leadership Development</h1>
+          </div>
+          <div className="container">
+            <div className="row text-left">
+              <div className="col">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              </div>
+              <div className="col">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              </div>
+            </div>
+          </div>
+          </div>
+        </section>
+
+        <section className="m-auto au-light-grey">
+          <div className="container">
+            <div className="icons d-flex justify-content-around p-3">
               <i className="fa fa-camera fa-3x p-3"></i>
-              <i className="fa fa-camera fa-3x p-3"></i>
-              <i className="fa fa-camera fa-3x p-3"></i>
+            </div>
+            <div className="d-flex justify-content-center text-center p-3">
+              <h1>Welcome to the Coach Approach for Leadership Development</h1>
+            </div>
+            <div className="container">
+            <div className="row text-left">
+              <div className="col">
+                <p className="w-75 m-auto">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              </div>
+              </div>
+            </div>
+
+            <div className="container">
+              <div className="row text-left">
+                <div className="col-sm m-3">
+                  <div className="my-dark-grey p-3 text-center text-white">
+                    <p className="lead">After our two weeks together, you will have</p>
+                  </div>
+                  <div className="au-white">
+                    <p className="p-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                  </div>
+                </div>
+                <div className="col-sm m-3">
+                  <div className="my-dark-grey p-3 text-center text-white">
+                    <p className="lead">After our two weeks together, you will have</p>
+                  </div>
+                  <div className="au-white">
+                    <p className="p-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </section>
+
+        <section className="m-auto my-dark-grey">
+          <div className="container container-dark">
+            <div className="icons d-flex justify-content-around mt-0">
+              <i className="fa fa-camera fa-3x p-3 light-icon"></i>
+            </div>
+          </div>
+        </section>
+
       </main>
       <footer>
       <p>This is the footer text</p>
--- my-app01/src/componets/header.js
+++ my-app01/src/componets/header.js
@@ -4,11 +4,6 @@ const Header = () => {
   return (
     <header className="App-header">
       <div className="img-center feature-img">
-        <img src="http://via.placeholder.com/1920x1080-?text=Picture+with+caption" className="img-fluid" alt="Placeholder Banner" />
-        <div className="d-none d-md-block text-left p-3">
-          <h3 className="pl-3"><strong>getty</strong>images</h3>
-          <p className="pl-3">Rawpixel</p>
-        </div>
       </div>
     </header>
   );
